6ff98a242c20cfdfac91da50efc5ebe676bff9e5
KhiemNC <nguyencongkhiem510@gmail.com>
2024-09-26 14:45:46 +0700

    Separate timer, we currently have a array of 10 timer.

---
+++ Core/Inc/software_timer.h
@@ -0,0 +1,24 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Sep 26, 2024
+ *      Author: nguye
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+#include "main.h"
+
+struct TimerStruct
+{
+	int counter;
+	int flag;
+};
+
+extern struct TimerStruct timer[10];
+
+void setTimer(int idx, int counter);
+void timerRun();
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
